4055ece3f57509f78b791560d799db6beeb73a8b
BlazeZheng <zhr0526@gmail.com>
2025-11-30 01:04:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 # 注意：Docker容器重启后，原本装好的包可能会消失（取决于容器类型），
 # 所以每次启动时运行一次安装命令是最保险的。pip会自动跳过已安装的包，不会浪费时间。
 echo "正在检查并安装依赖库..."
-pip install fastapi uvicorn mutagen requests
+pip install fastapi uvicorn mutagen requests pypinyin
 
 # 4. 启动播放器
 echo "正在启动播放器..."
